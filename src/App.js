475455d69356f64aa9eb7acf9f186c4f22b520ed
Annisa Eva <annisa.eat@gmail.com>
2022-06-17 14:40:52 +0700

    Atomic design

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, SafeAreaViewBase, StatusBar, Text} from 'react-native';
+
+
+const App = () => {
+  
+  return (
+    <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView>
+    <Text>Hello Market Tiga Lima App</Text>
+    </SafeAreaView>
+      
+    </>
+    
+  );
+};
+
+
+
+export default App;
